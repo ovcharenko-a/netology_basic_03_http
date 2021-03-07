89018214259f4d3991c211386b6aa29ec91bf98b
Anatoliy Ovcharenko <myeducationbox@inbox.ru>
2021-03-07 18:41:56 +0700

    feat(YaUploader): basic implementation

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+"""
+Работа с библиотекой requests, http-запросы
+"""
+import configparser
 from superhero import SuperHero
+from yandexupload import YaUploader
 
 def start_task(func):
     def wrapper():
@@ -11,16 +16,33 @@ def start_task(func):
 
 @start_task
 def task_1():
+    """
+    Определим, кто самый умный супергерой
+    """
     API = "2619421814940190"
     hulk = SuperHero("Hulk", API)
     captain_america = SuperHero("Captain America", API)
     thanos = SuperHero("Thanos", API)
     heroes = [hulk, captain_america, thanos]
     for hero in heroes:
-        hero.load_id()
+        hero.load_iq()
     heroes_iq = {hero.intelligence: hero for hero in heroes}
     key = max(heroes_iq.keys())
     print(f"Умнейший из героев {heroes_iq[key].name}, с интеллектом {heroes_iq[key].intelligence}")
 
+@start_task
+def task_2():
+    """
+    Загрузка файла на сервер
+    """
+    config = configparser.ConfigParser()  # создаём объекта парсера
+    config.read("token.conf")
+    API = config["main"]["yandex_token"]
+    uploader = YaUploader(API)
+    result = uploader.upload("helloworld.txt")
+    print(result)
+
+
 if __name__ == "__main__":
     task_1()
+    task_2()
---
+++ yandexupload.py
@@ -0,0 +1,52 @@
+"""Модуль описывающий класс YaUploader"""
+import requests
+from pathlib import Path
+
+class YaUploader:
+    """
+    Класс, реализующий загрузку файла на Яндекс диск.
+    """
+    HOST = "https://cloud-api.yandex.net:443"
+    RESULT_KEY = {
+        200: "Успех",
+        201: "Файл был загружен без ошибок.",
+        202: "Accepted — файл принят сервером, но еще не был перенесен непосредственно в Яндекс.Диск.",
+        412: "Precondition Failed — при дозагрузке файла был передан неверный диапазон в заголовке Content-Range.",
+        413: "Payload Too Large — размер файла превышает 10 ГБ.",
+        500: "Internal Server Error или 503 Service Unavailable — ошибка сервера, попробуйте повторить загрузку.",
+        507: "Insufficient Storage — для загрузки файла не хватает места на Диске пользователя."
+    }
+
+    def __init__(self, token: str):
+        self.token = token
+
+
+    def upload(self, input_file_path: str, remote_file_path: str = "", overwrite: bool = True):
+        """Метод загруджает файл file_path на яндекс диск"""
+        if not remote_file_path:
+            remote_file_path = Path(input_file_path).name
+        host_get = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+        headers = {
+            "Authorization": "OAuth " + self.token,
+            "User-Agent": "netology-student",
+            "Content - Type": "application/json",
+        }
+        params = {
+            "path": remote_file_path,
+            "overwrite": overwrite
+        }
+        preload_response = requests.get(host_get, params=params, headers=headers)
+        upload_href = preload_response.json().get("href", None)
+        if upload_href:
+            try:
+                with open(input_file_path, "rb") as file_head:
+                    response_upload = requests.put(upload_href, data=file_head)
+            except FileNotFoundError:
+                return f"ошибка чтения файла {input_file_path}"
+            return self.RESULT_KEY[response_upload.status_code]
+        else:
+            return (preload_response.json()["message"])
+
+
+
+
